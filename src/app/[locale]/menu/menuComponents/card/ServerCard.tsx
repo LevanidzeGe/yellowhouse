90f2e02ac9eefa22a1668a36fd16accd7240e0b8
levanydze <66974840+levanydze@users.noreply.github.com>
2024-11-07 10:49:16 +0100

    1

--- src/app/[locale]/menu/menuComponents/card/ServerCard.tsx
+++ src/app/[locale]/menu/menuComponents/card/ServerCard.tsx
@@ -60,7 +60,7 @@ export default function ServerCard({
           <div className={styles.details}>
             {option1 && <p className={styles.new}>new</p>}
             {option2 && <p className={styles.favorite}>favorite</p>}
-            {option3 && <p className={styles.vegan}>vegan</p>}
+            {option3 && <p className={styles.vegan}>vegetarian</p>}
           </div>
         </div>
       </div>
--- src/app/[locale]/menu/menuComponents/menu.tsx
+++ src/app/[locale]/menu/menuComponents/menu.tsx
@@ -20,25 +20,40 @@ export default async function Menu() {
       </div>
     );
   }
+  // Sort and filter categories based on priority and availability of enabled items
+  const sortedCategories = categories
+    .map((category) => {
+      // Check if category.items exists and has values
+      if (!category.items) return null;
 
-  // Sort categories based on priority
-  const sortedCategories = categories.sort((a, b) => a.priority - b.priority);
+      // Filter out disabled items for each category
+      const sortedItems = Object.values(category.items)
+        .filter((item: MenuItemProps) => !item.disable)
+        .sort((a: MenuItemProps, b: MenuItemProps) =>
+          a && b ? a.priority - b.priority : 0
+        );
 
+      // Only return categories that have at least one enabled item
+      return sortedItems.length > 0 ? { ...category, sortedItems } : null;
+    })
+    .filter(
+      (category): category is NonNullable<typeof category> => category !== null
+    ) // Filter out null values
+    .sort((a, b) => (a && b ? a.priority - b.priority : 0)); // Added null check
+
+  // Rendering the sorted categories
   return (
     <div className={styles.cardWrapper}>
       <section className="section section-medium">
-        <div className={` container ${styles.container}`}>
+        <div className={`container ${styles.container}`}>
           {sortedCategories.map((category, index) => {
+            if (!category) return null; // Check if category is not null
+
             // Access category translations for the current locale, fallback to 'en'
             const categoryName =
               category.translations?.[locale] ||
               category.translations?.[defaultLocale] ||
               "Unnamed Category";
-
-            // Sort items by their priority before rendering them
-            const sortedItems = Object.values(category.items).sort(
-              (a: MenuItemProps, b: MenuItemProps) => a.priority - b.priority
-            );
 
             return (
               <div key={index} className={styles.menuWrapper}>
@@ -46,28 +61,24 @@ export default async function Menu() {
                   <span className="font2 heading4 color2 ">
                     {t("underCategory")}
                   </span>
-                  {/* <span className="heading3 white">{t("underCategory")}</span> */}
                   <p className="twoLines"></p>
                   <h3 className="font2 color4 heading2">{categoryName}</h3>
                 </div>
                 <div className={styles.itemContainer}>
-                  {sortedItems.map(
-                    (item: MenuItemProps) =>
-                      !item.disable && (
-                        <ServerCard
-                          key={item.id}
-                          id={item.id}
-                          image={item.image}
-                          names={item.names}
-                          descriptions={item.descriptions}
-                          price={item.price}
-                          option1={item.option1 || false}
-                          option2={item.option2 || false}
-                          option3={item.option3 || false}
-                          priority={item.priority}
-                        />
-                      )
-                  )}
+                  {category.sortedItems.map((item: MenuItemProps) => (
+                    <ServerCard
+                      key={item.id}
+                      id={item.id}
+                      image={item.image}
+                      names={item.names}
+                      descriptions={item.descriptions}
+                      price={item.price}
+                      option1={item.option1 || false}
+                      option2={item.option2 || false}
+                      option3={item.option3 || false}
+                      priority={item.priority}
+                    />
+                  ))}
                 </div>
               </div>
             );
