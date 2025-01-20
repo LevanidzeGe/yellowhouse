a05655a9980194b2033b54011557311e98da8e10
levanydze <66974840+levanydze@users.noreply.github.com>
2025-01-20 09:55:17 +0400

    1

--- Manager/info.tsx
+++ Manager/info.tsx
@@ -1,14 +1,14 @@
 export const companyDomain = "https://geojudogeneve.ch";
 export const companyName = "Geo Judo";
-export const phoneNumber: string = "+41 79 566 69 16";
-export const phoneNumber2: string = "+41 79 913 70 44";
-export const instagramUrl: string = "https://www.instagram.com/toisondorgeneve";
+export const phoneNumber: string = "";
+export const phoneNumber2: string = "";
+export const instagramUrl: string = "";
 export const facebookUrl: string = "";
 export const foodUrl: string = "";
 export const linkedinUrl: string = "";
 export const googleUrl: string = "";
-export const address: string = "Square des Nénuphars 1, 1219 Vernier";
-export const addressLink: string = "https://maps.app.goo.gl/R8rEehPaT3jrJM5o9";
+export const address: string = "";
+export const addressLink: string = "";
 
-export const email: string = "toisondor.mari@gmail.com";
+export const email: string = "";
 //must fix
