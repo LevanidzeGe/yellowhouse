0fe77d4e5cc6065fc08fdf7e5a72d67d42871dc9
levanydze <66974840+levanydze@users.noreply.github.com>
2025-03-30 19:54:45 +0200

    1

--- lib/firebase/firebase.ts
+++ lib/firebase/firebase.ts
@@ -8,15 +8,14 @@ import { getStorage } from "firebase/storage";
 
 // ✅ Firebase Configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCsTZTmCOnDqfYD3kcQ07YC97hJ5KZV3tc",
-  authDomain: "fresh-2025-storage.firebaseapp.com",
-  databaseURL:
-    "https://fresh-2025-storage-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "fresh-2025-storage",
-  storageBucket: "fresh-2025-storage.firebasestorage.app",
-  messagingSenderId: "289622052541",
-  appId: "1:289622052541:web:a56e3772d27604fc0d71d4",
-  measurementId: "G-WLD539E4DT",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // 🔥 Initialize Firebase
